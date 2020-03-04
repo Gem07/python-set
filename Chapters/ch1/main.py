86d4cd6c5e8416ce5a69bb75cd3d2e86f132b7e0
Gem <dupe.adelaj@gmail.com>
2020-03-04 15:32:23 -0800

    Code for new Challenge

--- Chapters/ch1/main.py
+++ Chapters/ch1/main.py
@@ -1,10 +1,9 @@
-def challenge2():
+def challenge1():
 
-    season_of_the_year = input ('Enter a season: ')
-    whole_number = input ('Enter a whole number: ')
-    adjective = input ('Enter an adjective: ')
+    first_name = input('Enter your fist name: ')
+    last_name = input('Enter a whole number: ')
 
-    print(f'On a {adjective} {season_of_the_year} day, I drink a minimum of {whole_number} cups of coffee.')
+    print(f'Your full name is {first_name} {last_name}.')
 
 if __name__ == '__main__':
-    challenge2()
+    challenge1()
