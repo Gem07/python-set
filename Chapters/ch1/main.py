85dcb9ed8df0d5f3b2e892596db5d5930bf278ea
Gem <dupe.adelaj@gmail.com>
2020-03-03 18:23:16 -0800

    Code for new Challenge 2

---
+++ Chapters/ch1/main.py
@@ -0,0 +1,10 @@
+def challenge2():
+
+    season_of_the_year = input ('Enter a season: ')
+    whole_number = input ('Enter a whole number: ')
+    adjective = input ('Enter an adjective: ')
+
+    print(f'On a {adjective} {season_of_the_year} day, I drink a minimum of {whole_number} cups of coffee.')
+
+if __name__ == '__main__':
+    challenge2()
