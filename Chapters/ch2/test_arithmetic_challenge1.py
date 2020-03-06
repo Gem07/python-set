0e404fa43073fb4d6d31c04d11863770dae419db
Gem <dupe.adelaj@gmail.com>
2020-03-06 14:14:19 -0800

    Ch2 - Challenge 1
    Adding a positive and negative number

--- Chapters/ch2/test_arithmetic_challenge1.py
+++ Chapters/ch2/test_arithmetic_challenge1.py
@@ -1,13 +1,26 @@
-# Adding a positive and a negative number returns a positive number
+# Adding a positive and a negative number returns a positive number if the positive number was greater
+
 
 def add(x, y):
     return x + y
 
 
-def testing_adding_postive_negative_numbers():
-    number = add(4, -2)
-    assert number == 2
+def test_adding_positive_negative_numbers():
+    x = 100
+    y = -200
+    numb = add(x, y)
+    # assert numb == -160
+    if x > y:
+        print(f'\n \n The output, {numb} is positive.')
+    else:
+        print(f'\n\n The output is {numb} is negative.')
 
-if x > y:
-    print(f'The output, {number} is positive')
+
+
+# num1 = 3
+# num2 = -2
+# number = add(num1, num2)
+
+# if num1 > num2:
+#     print(f'The output, {number} is positive.')
 
