e143b145af86c3fc17ad75e6642d365191a98284
Gem <dupe.adelaj@gmail.com>
2020-03-06 15:41:04 -0800

    Update to Ch2 - challenge1.1
    adding 2 -ve numbs

--- Chapters/ch2/test_arithmetic_challenge1.1.py
+++ Chapters/ch2/test_arithmetic_challenge1.1.py
@@ -9,14 +9,14 @@ def test_adding_two_negative_numbers():
     numb = add(-2, -2)
     assert numb == -4
 
-
-num1 = -1
-num2 = 5
-number = add(num1, num2)
-
-if num1 < 0 and num2 < 0:
-    print(f'The output, {number} is negative')
-else:
-    print(f'The output, {number} is positive.')
+#
+# num1 = -1
+# num2 = 5
+# number = add(num1, num2)
+#
+# if num1 < 0 and num2 < 0:
+#     print(f'The output, {number} is negative')
+# else:
+#     print(f'The output, {number} is positive.')
 
 
