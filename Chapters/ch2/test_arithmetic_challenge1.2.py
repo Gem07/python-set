794a68d0725405fa897488794dc79263daa089fa
Gem <dupe.adelaj@gmail.com>
2020-03-06 15:46:00 -0800

    Update to Ch2 - challenge1.2

--- Chapters/ch2/test_arithmetic_challenge1.2.py
+++ Chapters/ch2/test_arithmetic_challenge1.2.py
@@ -7,13 +7,15 @@ def add(x, y):
 
 
 def test_adding_zero_to_whole_numbers():
-    numb = add(90, 0)
-    assert numb == 90
+    x = 0
+    y = -200
+    numb = add(x, y)
+    assert numb == -200
 
 
-num1 = 0
-num2 = 18
-number = add(num1, num2)
-
-if num1 > 0 or num2 > 0:
-    print(f'The sum {number} is always equal to the whole number.')
+# num1 = 0
+# num2 = 18
+# number = add(num1, num2)
+#
+# if num1 > 0 or num2 > 0:
+#     print(f'The sum {number} is always equal to the whole number.')
