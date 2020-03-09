668797ea0845fc45c1b48e5d58bd5d30cc089820
Gem <dupe.adelaj@gmail.com>
2020-03-08 21:32:44 -0700

    Code for Ch2 - Challenge1

--- Chapters/ch2/test_arithmetic_challenge1.py
+++ Chapters/ch2/test_arithmetic_challenge1.py
@@ -1,4 +1,4 @@
-# Adding a positive and a negative number returns a positive number if the positive number was greater
+# Theory 1: Adding a positive and a negative number returns a positive number if the positive number was greater
 
 
 def add(x, y):
@@ -6,9 +6,25 @@ def add(x, y):
 
 
 def test_adding_positive_negative_numbers():
-    x = 100
+    x = 1000
     y = -200
     numb = add(x, y)
-    assert numb == -100
+    assert numb == 800
+
+# Theory2: Adding two negative numbers returns a negative number
+
+
+def test_adding_two_negative_numbers():
+    numb = add(-12, -20)
+    assert numb == -32
+
+# Theory 3: Adding zero to any whole number returns that whole number
+
+
+def test_adding_zero_to_whole_numbers():
+    x = 0
+    y = -200
+    numb = add(x, y)
+    assert numb == -200
 
 
