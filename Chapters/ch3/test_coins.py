8715a5332d0088b3a2985b0b4389d99b8dc022d2
Gem <dupe.adelaj@gmail.com>
2020-04-04 17:55:32 -0700

    Update to Ch3

--- Chapters/ch3/test_coins.py
+++ Chapters/ch3/test_coins.py
@@ -3,26 +3,28 @@ print("~~~~~Coin Challenge~~~~~")
 
 def test_sum_pennies():
     user_input = '15'
-    numb_of_pennies = int(user_input)
+    numb_of_pennies = int(user_input) * 1
+#   sum_of_coins = numb_of_pennies * 1
     assert numb_of_pennies == 15
+#   assert sum_of_coins ==
 
 
 def test_sum_nickels():
     user_input = '4'
-    numb_of_nickels = int(user_input)
-    assert numb_of_nickels == 4
+    numb_of_nickels = int(user_input) * 5
+    assert numb_of_nickels == 20
 
 
 def test_sum_dimes():
     user_input = '4'
-    numb_of_dimes = int(user_input)
-    assert numb_of_dimes == 4
+    numb_of_dimes = int(user_input) * 10
+    assert numb_of_dimes == 40
 
 
 def test_sum_quarters():
     user_input = '1'
-    numb_of_quarters = int(user_input)
-    assert numb_of_quarters == 1
+    numb_of_quarters = int(user_input) * 25
+    assert numb_of_quarters == 25
 
 
 def test_sum_all_coins():
@@ -32,12 +34,12 @@ def test_sum_all_coins():
 
 
 def test_total_under_dollar():
-    user_input = '15'
-    value_under = int(user_input)
+    user_input = '85'
+    value_under = 100 - int(user_input)
     assert value_under == 15
 
 
 def test_total_over_dollar():
-    user_input = '22'
-    excess_value = int(user_input)
-    assert excess_value == 22
+    user_input = '107'
+    excess_value = int(user_input) - 100
+    assert excess_value == 7
