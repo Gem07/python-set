4c0a971d69fc2688af4fcbe04eefdd8361345806
Gem <dupe.adelaj@gmail.com>
2020-04-26 21:29:11 -0700

    Code for test_coins

--- Chapters/ch3/test_coins.py
+++ Chapters/ch3/test_coins.py
@@ -1,45 +1,45 @@
-print("~~~~~Coin Challenge~~~~~")
-
 
 def test_sum_pennies():
-    user_input = '15'
-    value_of_pennies = int(user_input) * 1
-#   sum_of_coins = numb_of_pennies * 1
-    assert value_of_pennies == 15
-#   assert sum_of_coins ==
+    user_input = '20'
+    total_value_of_pennies = int(user_input) * 1
+    assert total_value_of_pennies == 20
 
 
 def test_sum_nickels():
-    user_input = '4'
-    value_of_nickels = int(user_input) * 5
-    assert value_of_nickels == 20
+    user_input = '9'
+    total_value_of_nickels = int(user_input) * 5
+    assert total_value_of_nickels == 45
 
 
 def test_sum_dimes():
-    user_input = '4'
-    value_of_dimes = int(user_input) * 10
-    assert value_of_dimes == 40
+    user_input = '1'
+    total_value_of_dimes = int(user_input) * 10
+    assert total_value_of_dimes == 10
 
 
 def test_sum_quarters():
     user_input = '1'
-    value_of_quarters = int(user_input) * 25
-    assert value_of_quarters == 25
+    total_value_of_quarters = int(user_input) * 25
+    assert total_value_of_quarters == 25
 
 
-def test_sum_all_coins():
-    user_input = '100'
-    total_number_of_coins = int(user_input)
-    assert total_number_of_coins == 100
+def test_total_value():
+    input_sum_of_coins = '100'
+    if int(input_sum_of_coins) == 100:
+        assert int(input_sum_of_coins) == 100
 
 
-def test_total_under_dollar():
-    user_input = '85'
-    value_under = 100 - int(user_input)
-    assert value_under == 15
+def test_total_value_over():
+    input_sum_of_coins = '120'
+    if int(input_sum_of_coins) > 100:
+        value_over = int(input_sum_of_coins) - 100
+        assert value_over == 20
 
 
-def test_total_over_dollar():
-    user_input = '107'
-    excess_amount = int(user_input) - 100
-    assert excess_amount == 7
+def test_total_value_under():
+    input_sum_of_coins = '87'
+    if int(input_sum_of_coins) < 100:
+        value_under = 100 - int(input_sum_of_coins)
+        assert value_under == 13
+
+
